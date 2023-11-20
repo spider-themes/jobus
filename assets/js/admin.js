1b45bc13ea3ae77114f9c486915b87beb19ea172
Delwer Hossain <delweratjk@gmail.com>
2023-11-20 21:27:56 +0600

    Meta key insert automatically
    Meta key insert automatically based on the meta name

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,14 +5,40 @@
 
         // Disabled already exist key field [ Job Specifications ]
         $('.csf-cloneable-wrapper[data-field-id="[job_specifications]"] .csf-cloneable-item').each(function() {
-            var metaKey = $(this).find('input[data-depend-id="meta_key"]').val();
+            var metaKey     = $(this).find('input[data-depend-id="meta_key"]').val();
+            var $container  = $('.csf-cloneable-wrapper[data-field-id="[job_specifications]"]');
+
+            // add attr each item 
+            var newIndex    = $(this).index() + 1; 
+            $(this).attr('cloned-item-id', newIndex);
            
             if (metaKey) {
                 $(this).find('input[data-depend-id="meta_key"]').attr('readonly', true);
+            } else {
+                $container.find('.csf-cloneable-item[cloned-item-id='+newIndex+'] input[data-depend-id="meta_name"]').keyup(function(){
+                    $container.find('.csf-cloneable-item[cloned-item-id='+newIndex+'] input[data-depend-id="meta_key"]').val($(this).val().replace(/\s+/g, '-').toLowerCase());
+                });
             }
 
         });
-        // ended
+        
+        // Meta key automatically insert [ Job Specifications ]
+        $('.csf-cloneable-add').on('click', function () {
+            var $container  = $('.csf-cloneable-wrapper[data-field-id="[job_specifications]"]');
+            var $lastItem   = $container.find('.csf-cloneable-item').last();
+            
+            // Index is zero-based, so we add 1 for the next index
+            var newIndex    = $lastItem.index() + 1; 
+
+            // Index is used as attribute
+            $lastItem.attr('cloned-item-id', newIndex);
+            
+            $container.find('.csf-cloneable-item[cloned-item-id='+newIndex+'] input[data-depend-id="meta_name"]').keyup(function(){
+                $container.find('.csf-cloneable-item[cloned-item-id='+newIndex+'] input[data-depend-id="meta_key"]').val($(this).val().replace(/\s+/g, '-').toLowerCase());
+            });
+            
+        });
+        
     });
 
 })(jQuery);
