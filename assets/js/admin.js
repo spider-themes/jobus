753b41073e9422e42717b6768e84a7a69dba1467
Delwer Hossain <delweratjk@gmail.com>
2024-12-28 15:26:51 +0600

    fixed meta key value based on meta name field

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -24,22 +24,27 @@
 
         });
         
-        // Meta-key automatically inserts [ Job Specifications ]
         $('.csf-cloneable-add').on('click', function () {
-            var $container  = $('.csf-cloneable-wrapper[data-field-id="[job_specifications]"], .csf-cloneable-wrapper[data-field-id="[company_specifications]"], .csf-cloneable-wrapper[data-field-id="[candidate_specifications]"]');
-            var $lastItem   = $container.find('.csf-cloneable-item').last();
-            
+            var $container = $('.csf-cloneable-wrapper[data-field-id="[job_specifications]"], .csf-cloneable-wrapper[data-field-id="[company_specifications]"], .csf-cloneable-wrapper[data-field-id="[candidate_specifications]"]');
+            var $lastItem = $container.find('.csf-cloneable-item').last();
+        
             // Index is zero-based, so we add 1 for the next index
-            var newIndex    = $lastItem.index() + 1; 
-
-            // Index is used as attribute
+            var newIndex = $lastItem.index() + 1;
+        
+            // Index is used as an attribute
             $lastItem.attr('cloned-item-id', newIndex);
-            
-            $container.find('.csf-cloneable-item[cloned-item-id='+newIndex+'] input[data-depend-id="meta_name"]').keyup(function(){
-                $container.find('.csf-cloneable-item[cloned-item-id='+newIndex+'] input[data-depend-id="meta_key"]').val($(this).val().replace(/\s+/g, '-').toLowerCase());
-            });
-            
         });
+        
+        // Delegate the event handling for dynamically added elements
+        $(document).on('keyup', '.csf-cloneable-item input[data-depend-id="meta_name"]', function () {
+            var $currentItem = $(this).closest('.csf-cloneable-item');
+            var newIndex = $currentItem.attr('cloned-item-id');
+        
+            $currentItem.find('input[data-depend-id="meta_key"]').val(
+                $(this).val().replace(/\s+/g, '-').toLowerCase()
+            );
+        });
+        
         
     });
 
