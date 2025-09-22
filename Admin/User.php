631d4d2181f17a05af5dac6d8ec99f70ea46c22a
Eh Jewel <ehjewel2@gmail.com>
2025-09-22 18:49:47 +0600

    Removed duplicate !defined( 'ABSPATH' )

--- Admin/User.php
+++ Admin/User.php
@@ -1,7 +1,4 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
 /**
  * User Management for Jobus Plugin
  *
--- Admin/cpt/Company.php
+++ Admin/cpt/Company.php
@@ -1,7 +1,4 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
 
 namespace jobus\Admin\cpt;
 
